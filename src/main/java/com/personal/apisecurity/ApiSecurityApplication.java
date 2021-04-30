b27ced58d92e968b92c47a77157e4159c59f949f
marcos <marcosfilipe.dm@gmail.com>
2021-04-30 10:24:25 -0300

    feat: :sparkles: Alter websecurity class, add jwt authentication.

--- src/main/java/com/personal/apisecurity/ApiSecurityApplication.java
+++ src/main/java/com/personal/apisecurity/ApiSecurityApplication.java
@@ -2,9 +2,6 @@ package com.personal.apisecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ApiSecurityApplication {
@@ -13,8 +10,8 @@ public class ApiSecurityApplication {
 		SpringApplication.run(ApiSecurityApplication.class, args);
 	}
 
-	@Bean 
+	/*@Bean 
 	public PasswordEncoder passwordEncoder() { 
 	    return new BCryptPasswordEncoder(); 
-	}
+	}*/
 }
